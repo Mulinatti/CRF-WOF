0f35c71ae6d1a0d6603b24c08670c9f6ba7adca4
mulinatti <gabrielmulinari2002@gmail.com>
2024-01-25 04:02:12 -0300

    desktop ui created

--- src/pages/PlayerPage/index.tsx
+++ src/pages/PlayerPage/index.tsx
@@ -2,24 +2,26 @@ import { ArrowRight, ChevronLeft } from "lucide-react";
 
 const PlayerPage = () => {
   return (
-    <main className="p-4" style={{background: "linear-gradient(180deg, rgba(153,27,27,1) 0%, rgba(11,11,11,1) 50%)"}}>
-      <div className="flex items-center">
+    <main className="overflow-hidden">
+      <div className="flex items-start w-full pt-2 h-screen absolute" style={{background: "linear-gradient(180deg, rgba(153,27,27,1) 0%, rgba(11,11,11,1) 50%)"}}>
         <ChevronLeft strokeWidth={3} size={28} />
         <p className="uppercase translate-y-[1px] font-bold">Back</p>
       </div>
-      <section className="mt-5">
+      <div className="p-4">
+      <section className="mt-8">
         <div className="relative">
           <div>
             <img
-              className="invert absolute -top-0 -translate-x-6 -translate-y-6 w-4/5 opacity-10"
+              className="invert absolute -top-0 -translate-x-1/2 left-1/2 -translate-y-6 w-[260px] opacity-10"
               src="../logos/logo.svg"
               alt="asda"
             />
           </div>
           <div>
             <img
-              className="absolute w-[200px] h-[300px] -right-16"
-              src="../images/cr7.png"
+              //A PROPORÇAO DE ALTURA E LARGURA É CALCULADA POR WIDTH x 1,33 = ALTURA
+              className="absolute w-[200px] h-[266px] transition-all md:w-[300px] md:h-[400px] top-10 md:top-0 -right-16 md:right-0"
+              src="../images/cr72.png"
               alt=""
             />
           </div>
@@ -33,7 +35,7 @@ const PlayerPage = () => {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-3 text-sm z-50 relative *:border *:border-white/10 *:rounded-lg *:p-2 *:bg-zinc-900/50 *:backdrop-blur-sm gap-3">
+        <div className="grid grid-cols-3 text-sm z-10 relative *:border *:border-white/10 *:rounded-lg *:p-2 *:bg-zinc-900/50 *:backdrop-blur-sm gap-3">
           <div className="flex flex-col justify-between h-20">
             <span>Goals</span>
             <span className="text-end text-2xl font-bold">6</span>
@@ -48,25 +50,50 @@ const PlayerPage = () => {
           </div>
         </div>
       </section>
-      <section className="mt-6">
+      <section className="backdrop-blur-sm py-5">
         <div className="flex">
           <ArrowRight color="green" strokeWidth={3} size={20}/>
-          <p className="ml-2 text-sm">10/06/2023</p>
+          <p className="text-sm ml-3">10/06/2023</p>
         </div>
       </section>
-      <section className="mt-6 bg-zinc-900/50 rounded-lg border border-white/10 p-2">
+      <section className="backdrop-blur-sm bg-zinc-900/50 rounded-lg border border-white/10 p-2">
         <h3 className="ml-1 text-xs bg-zinc-900 p-1 px-2 rounded-lg w-fit">Campeonatos</h3>
-        <div className="flex flex-col items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3">
           <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
             <img className="w-10" src="../images/trophy.png" alt="" />
-            <p className="text-sm">Weekly - 20/12/2023</p>
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
           </div>
           <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
             <img className="w-10" src="../images/trophy.png" alt="" />
-            <p className="text-sm">Weekly - 20/12/2023</p>
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
+          </div>
+          <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
+            <img className="w-10" src="../images/trophy.png" alt="" />
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
+          </div>
+          <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
+            <img className="w-10" src="../images/trophy.png" alt="" />
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
+          </div>
+          <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
+            <img className="w-10" src="../images/trophy.png" alt="" />
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
+          </div>
+          <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
+            <img className="w-10" src="../images/trophy.png" alt="" />
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
+          </div>
+          <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
+            <img className="w-10" src="../images/trophy.png" alt="" />
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
+          </div>
+          <div className="flex flex-col items-center border-b border-white/10 w-full py-4">
+            <img className="w-10" src="../images/trophy.png" alt="" />
+            <p className="text-sm text-center mt-2">Weekly - 20/12/2023</p>
           </div>
         </div>
       </section>
+      </div>
     </main>
   );
 };
